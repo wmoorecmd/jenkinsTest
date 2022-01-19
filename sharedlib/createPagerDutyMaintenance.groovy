23ad00ae244492a3bce6e15962d0e31694d59e9a
wmoorecmd <wmoore@cognisantmd.com>
2022-01-19 10:23:34 -0500

    Finished up groovy file

--- sharedlib/createPagerDutyMaintenance.groovy
+++ sharedlib/createPagerDutyMaintenance.groovy
@@ -2,22 +2,37 @@
  * Creates a PagerDuty Maintenance window
  *
  * Arguments
- *  - env: dev or prod
+ *  - env: dev prod
  *  - token: the PagerDuty API Token
  *
 */
 
-def call(Map pagerDutyArgs){
-    def service_id = ""
-    def requester_user_id = ""
-
-    if(pagerDutyArgs.env == 'prod'){
-        //prod pagerduty info goes here
-    } else if(pagerDutyArgs.env == 'dev'){
-        //dev pagerduty info goes here
-        service_id = "P77YHJX"
-        requester_user_id = "PYSN9IH"
-    } else {
-        // what
+Boolean call(Map pagerDutyArgs){
+    //return false when any argument is missing
+    if(!(pagerDutyArgs.containsKey('env') && pagerDutyArgs.containsKey('token'))){
+        return false
     }
+
+    String extras = "api_token=${pagerDutyArgs.token}"
+
+    //build the extra argument string
+    if(pagerDutyArgs.env == 'prod'){
+        //TODO use actual values here
+        extras += ' service_id=000000'
+        extras += ' requester_user_id=000000'
+    } else if(pagerDutyArgs.env == 'dev'){
+        extras += ' service_id=P77YHJX'
+        extras += ' requester_user_id=PYSN9IH'
+    } else {
+        //TODO maybe create a class to get an error message back
+        return false
+    }
+
+    //run ansible
+    ansiblePlaybook(
+            playbook: 'task-pagerduty-maintenance.yml',
+            extras: extras
+    )
+    
+    return true
 }
