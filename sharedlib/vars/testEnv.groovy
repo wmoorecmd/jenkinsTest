48ef347a33bbff7403e1b9197390c134311810de
wmoorecmd <wmoore@cognisantmd.com>
2022-05-17 16:25:58 -0400

    add one thing

--- sharedlib/vars/testEnv.groovy
+++ sharedlib/vars/testEnv.groovy
@@ -1,9 +1,5 @@
 def call(Map testMap = [region: "ca-central-1", testString: "this is a test"]){
-    String region = ""
-    if(testMap.containsKey("testKey")){
-        region = testMap.testKey
-    }
-    withEnv(["AWS_DEFAULT_REGION=${region}"]){
+    withEnv(["AWS_DEFAULT_REGION=${testMap.region}"]){
         withCredentials([string(credentialsId: 'SUPER_SECRET', variable: 'THE_SECRET')]){
             sh 'echo $THE_SECRET'
             sh 'echo $AWS_DEFAULT_REGION'
