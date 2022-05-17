783da41f566ddc0df2b28a6e808cfdbf21efe336
wmoorecmd <wmoore@cognisantmd.com>
2022-05-17 15:01:18 -0400

    add env test stuff

--- sharedlib/vars/testEnv.groovy
+++ sharedlib/vars/testEnv.groovy
@@ -1,7 +1,8 @@
 def call(){
-    withCredentials([string(credentialsId: 'SUPER_SECRET', variable: 'THE_SECRET')]){
-        sh 'echo $THE_SECRET'
+    withEnv(['THE_ENV_VAR=thisisavariable']){
+        withCredentials([string(credentialsId: 'SUPER_SECRET', variable: 'THE_SECRET')]){
+            sh 'echo $THE_SECRET'
+            sh 'echo $THE_ENV_VAR'
+        }
     }
-
-    return '$THE_SECRET'
 }
